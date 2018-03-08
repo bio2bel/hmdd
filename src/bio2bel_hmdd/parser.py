828201fd3b5f88ecbbdc85e12772ef9e589122fe
Mehdi <mehdi.ali@scai.fraunhofer.de>
2018-03-08 11:08:17 +0100

    Add download stub

--- src/bio2bel_hmdd/parser.py
+++ src/bio2bel_hmdd/parser.py
@@ -1,1 +1,10 @@
 # -*- coding: utf-8 -*-
+
+
+def download_stuff(force_download=False):
+    """Download the HMDD database as a TSV
+
+    :param bool force_download: If true, forces the data to get downloaded again
+    :return: The system file path of the downloaded file
+    :rtype: str
+    """
