ba37c2907c63eeaa7eb4b74042a49202a6bce890
Dejan Dukic <dejan.dukic@scai.fraunhofer.de>
2018-03-08 11:26:52 +0100

    Implement the downloader

--- src/bio2bel_hmdd/parser.py
+++ src/bio2bel_hmdd/parser.py
@@ -1,10 +1,25 @@
 # -*- coding: utf-8 -*-
 
+import logging
+import os
+from urllib.request import urlretrieve
 
-def download_stuff(force_download=False):
+from .constants import DATA_FILE_PATH, DATA_URL
+
+log = logging.getLogger(__name__)
+
+
+def download_hmdd(force_download=False):
     """Download the HMDD database as a TSV
 
-    :param bool force_download: If true, forces the data to get downloaded again
+    :param bool force_download: If true, forces the data to get downloaded again; defaults to False
     :return: The system file path of the downloaded file
     :rtype: str
     """
+    if os.path.exists(DATA_FILE_PATH) and not force_download:
+        log.info('using cached data at %s', DATA_FILE_PATH)
+    else:
+        log.info('downloading %s to %s', DATA_URL, DATA_FILE_PATH)
+        urlretrieve(DATA_URL, DATA_FILE_PATH)
+
+    return DATA_FILE_PATH
